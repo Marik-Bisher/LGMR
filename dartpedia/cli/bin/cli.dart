b863901fde78bd69734670965af5dd398806cf09
Marik_Bisher <el.gi.00@mail.ru>
2026-05-21 18:40:53 +0300

    Исправлен вызов searchWikipedia с правильной передачей аргументов

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,20 +1,22 @@
-
 const version = '0.0.1';
+
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    
-    print('Search command recognized!');
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);  // ← ВЫЗОВ
   } else {
     printUsage();
   }
 }
+
 void searchWikipedia(List<String>? arguments) { 
   print('searchWikipedia received arguments: $arguments');
 }
+
 void printUsage() { 
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
