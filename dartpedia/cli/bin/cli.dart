ed8b50a6f13ba582443e53620c14c5459e0bd50e
Marik_Bisher <el.gi.00@mail.ru>
2026-05-27 19:43:07 +0300

    Проверка ввода и подключение посика в Вики

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -32,23 +32,28 @@ Future<String> getWikipediaArticle(String articleTitle) async {
 }
 
 
-void searchWikipedia(List<String>? arguments) async { 
-  final String? articleTitle;
 
-  
+void searchWikipedia(List<String>? arguments) async {
+  final String articleTitle;
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
-    articleTitle = stdin.readLineSync(); 
-    
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
-    
     articleTitle = arguments.join(' ');
   }
 
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+
+  
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent); 
 }
+
 
 void printUsage() { 
   print(
