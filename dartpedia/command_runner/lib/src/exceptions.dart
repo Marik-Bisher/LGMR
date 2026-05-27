0c746e0166011958cfd613b1a086222ba1e61048
Marik_Bisher <el.gi.00@mail.ru>
2026-05-27 20:57:45 +0300

    Создание файла exceptions.dart и определение класса ArgumentException для обозначения ошибок, связанный с аргументами командной строки

---
+++ dartpedia/command_runner/lib/src/exceptions.dart
@@ -0,0 +1,20 @@
+class ArgumentException extends FormatException {
+  
+  final String? command;
+
+  
+  final String? argumentName;
+
+  ArgumentException(
+    super.message, [
+    this.command,
+    this.argumentName,
+    super.source,
+    super.offset,
+  ]);
+
+  @override
+  String toString() {
+    return 'ArgumentException: $message';
+  }
+}
